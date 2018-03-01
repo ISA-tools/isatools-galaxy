67796a8ee2f867da1c94b4319e3fd21c410bb9c9
djcomlab <djcomlab@gmail.com>
2018-03-01 12:20:44 +0000

    Updates to isatab_validator tool, not sure why test runner won't run though

--- tools/validators/run_validator.py
+++ tools/validators/run_validator.py
@@ -3,21 +3,31 @@ import glob
 import json
 import os
 import sys
+import zipfile
+import tempfile
 
-isa_path = sys.argv[1]
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
 try:
     from isatools import isatab
 except ImportError as e:
     raise RuntimeError('Could not import isatools.isatab package')
-if not os.path.exists(isa_path):
-    print('File path to ISA files \'{}\' does not exist'.format(isa_path))
+
+tmp_dir = tempfile.mkdtemp()
+with zipfile.ZipFile(input_path) as zfp:
+    zfp.extractall(path=tmp_dir)
+if not os.path.exists(tmp_dir):
+    print('File path to ISA files \'{}\' does not exist'.format(tmp_dir))
     sys.exit(0)
 report = None
-i_files = glob.glob(os.path.join(isa_path, 'i_*.txt'))
+i_files = glob.glob(os.path.join(tmp_dir, 'i_*.txt'))
 if len(i_files) == 1:
     i_file_name = next(iter(i_files))
     with open(i_file_name) as in_fp:
         json_report = isatab.validate(in_fp)
         if json_report is not None:
-            with open('report.json', 'w') as out_fp:
+            with open(output_path, 'w') as out_fp:
                 json.dump(json_report, out_fp, indent=4)
+import shutil
+shutil.rmtree(tmp_dir)
