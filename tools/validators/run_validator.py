4b4ee9c0e922e5f1a6be877ba1e777c7c20057d4
djcomlab <djcomlab@gmail.com>
2018-03-11 22:40:52 +0000

    Add html renderer for ISA-Tab validator report output

--- tools/validators/run_validator.py
+++ tools/validators/run_validator.py
@@ -7,7 +7,8 @@ import zipfile
 import tempfile
 
 input_path = sys.argv[1]
-output_path = sys.argv[2]
+json_output_path = sys.argv[2]
+html_output_path = sys.argv[3]
 
 try:
     from isatools import isatab
@@ -27,20 +28,22 @@ if len(i_files) == 1:
     with open(i_file_name) as in_fp:
         json_report = isatab.validate(in_fp)
         if json_report is not None:
-            with open(output_path, 'w') as out_fp:
+            with open(json_output_path, 'w') as out_fp:
                 json.dump(json_report, out_fp, indent=4)
 import shutil
 shutil.rmtree(tmp_dir)
 
 
 def build_messages(messages):
-    messages_table = "<table>"
-    messages_table += "<tr><th>Code</th><th>Message</th><th>Supplemental information</th></tr>"
-    for message in messages:
-        messages_table += "<tr><td>{code}</td><td>{message}</td><td>{supplemental}</td></tr>".format(
-            code=message['code'], message=message['message'], supplemental=message['suplemental']
-        )
-    messages_table += "</table>"
+    messages_table = "No messages."
+    if len(messages) > 0:
+        messages_table = "<table>"
+        messages_table += "<tr><th>Code</th><th>Message</th><th>Supplemental information</th></tr>"
+        for message in messages:
+            messages_table += "<tr><td>{code}</td><td>{message}</td><td>{supplemental}</td></tr>".format(
+                code=message['code'], message=message['message'], supplemental=message['supplemental']
+            )
+        messages_table += "</table>"
     return messages_table
 
 
@@ -77,5 +80,5 @@ report_html = """
     warnings_table=build_messages(json_report['warnings']),
     errors_table=build_messages(json_report['errors'])
 )
-
-print(report_html)
+with open(html_output_path, 'w') as html_fp:
+    html_fp.write(report_html)
