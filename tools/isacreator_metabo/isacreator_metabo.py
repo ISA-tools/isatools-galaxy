9ecf52f5af6c3272813fc7f36cf2347eb304f46b
djcomlab <djcomlab@gmail.com>
2018-05-03 15:55:32 +0100

    Refactor python wrapper

--- tools/isacreator_metabo/isacreator_metabo.py
+++ tools/isacreator_metabo/isacreator_metabo.py
@@ -27,33 +27,7 @@ from isatools.create.models import (
 from isatools.model import Investigation, Person
 
 
-@click.command()
-@click.option('--galaxy_parameters_file',
-              help='Path to JSON file containing input Galaxy JSON',
-              type=click.File())
-@click.option('--target_dir', help='Output path to write', nargs=1,
-              type=click.Path(exists=True), default='./')
-def create_from_galaxy_parameters(galaxy_parameters_file, target_dir):
-
-    # pre-generation checks
-    if galaxy_parameters_file:
-        galaxy_parameters = json.load(galaxy_parameters_file)
-        # print(json.dumps(galaxy_parameters, indent=4))  # fo debugging only
-    else:
-        raise IOError('Could not load Galaxy parameters file!')
-
-    if target_dir:
-        if not os.path.exists(target_dir):
-            raise IOError('Target path does not exist!')
-
-    if len(galaxy_parameters['sampling_and_assay_plans']) == 0:
-        raise IOError('No Sampling plan specified')
-
-    for sample_assay_plan in galaxy_parameters['sampling_and_assay_plans']:
-        if len(sample_assay_plan['assay_record_series']) == 0:
-            raise IOError('A Sampling plan does not contain any Assay plans')
-
-    # Treatments
+def _create_treatment_sequence(galaxy_parameters):
     treatment_plan = galaxy_parameters['treatment_plan']
     study_type = treatment_plan['study_type_cond']['study_type']
 
@@ -101,109 +75,120 @@ def create_from_galaxy_parameters(galaxy_parameters_file, target_dir):
                                            duration_of_exposure_level.strip())
     treatment_sequence = TreatmentSequence(
         ranked_treatments=treatment_factory.compute_full_factorial_design())
+    return treatment_sequence
 
-    # Sample-Assay Plan
-    sample_assay_plan = SampleAssayPlan()
-    for sample_plan_params in galaxy_parameters['sampling_and_assay_plans'][
-            'sample_record_series']:
-        sample_type = sample_plan_params['sample_type']['sample_type'] if \
-            not sample_plan_params['sample_type']['sample_type'] == \
-                'user_defined' else sample_plan_params[
-            'sample_type']['sample_type_ud']
+
+@click.command()
+@click.option('--galaxy_parameters_file',
+              help='Path to JSON file containing input Galaxy JSON',
+              type=click.File())
+@click.option('--target_dir', help='Output path to write', nargs=1,
+              type=click.Path(exists=True), default='./')
+def create_from_galaxy_parameters(galaxy_parameters_file, target_dir):
+
+    def _create_sample_plan(sample_assay_plan, sample_plan_record):
+
+        def _create_nmr_assay_type(assay_plan_record):
+            nmr_assay_type = AssayType(
+                measurement_type='metabolite profiling',
+                technology_type='nmr spectroscopy')
+            from isatools.create.models import NMRTopologyModifiers
+            nmr_top_mods = NMRTopologyModifiers()
+            nmr_top_mods.technical_replicates = assay_plan_record[
+                'assay_type']['acq_mod_cond']['technical_replicates']
+            nmr_top_mods.acquisition_modes.add(
+                assay_plan_record['assay_type']['acq_mod_cond']['acq_mod'])
+            nmr_top_mods.instruments.add('{} {}'.format(
+                assay_plan_record['assay_type']['acq_mod_cond']['nmr_instrument'],
+                assay_plan_record['assay_type']['acq_mod_cond']['magnet']))
+            nmr_top_mods.pulse_sequences.add(
+                assay_plan_record['assay_type']['acq_mod_cond']['pulse_seq']
+            )
+            nmr_top_mods.magnet_power = \
+                assay_plan_record['assay_type']['acq_mod_cond']['magnet']
+            nmr_assay_type.topology_modifiers = nmr_top_mods
+            return nmr_assay_type
+
+        def _create_ms_assay_type(assay_plan_record):
+            ms_assay_type = AssayType(
+                measurement_type='metabolite profiling',
+                technology_type='mass spectrometry')
+            ms_assay_type.topology_modifiers = MSTopologyModifiers(
+                sample_fractions=set(
+                    map(lambda x: x['fraction'], assay_plan_record['assay_type']['samp_frac_series'])))
+            injection_modes = ms_assay_type.topology_modifiers.injection_modes
+            if len(assay_plan_record['assay_type'][
+                       'inj_mod_series']) > 0:
+                for inj_mod in assay_plan_record['assay_type']['inj_mod_series']:
+                    injection_mode = MSInjectionMode(
+                        injection_mode=inj_mod['inj_mod_cond']['inj_mod'],
+                        ms_instrument=inj_mod['inj_mod_cond']['instrument']
+                    )
+                    if inj_mod['inj_mod_cond']['inj_mod'] in ('LC', 'GC'):
+                        injection_mode.chromatography_instrument = inj_mod['inj_mod_cond']['chromato']
+                        if inj_mod['inj_mod_cond']['inj_mod'] == 'LC':
+                            injection_mode.chromatography_column = inj_mod['inj_mod_cond']['chromato_col']
+                    injection_modes.add(injection_mode)
+                    for acq_mod in inj_mod['inj_mod_cond']['acq_mod_series']:
+                        injection_mode.acquisition_modes.add(
+                            MSAcquisitionMode(
+                                acquisition_method=acq_mod['acq_mod'],
+                                technical_repeats=acq_mod[
+                                    'technical_replicates']
+                            )
+                        )
+                        if inj_mod['inj_mod_cond']['inj_mod'] == 'GC':
+                            for deriva in inj_mod['inj_mod_cond']['derivatization_series']:
+                                injection_mode.derivatizations.add(deriva['derivatization'])
+            return ms_assay_type
+
+        if sample_plan_record['sample_type']['sample_type'] == 'user defined':
+            sample_type = sample_plan_record['sample_type']['sample_type_ud']
+        else:
+            sample_type = sample_plan_record['sample_type']['sample_type']
         sample_assay_plan.add_sample_type(sample_type)
-        sample_size = sample_plan_params['sample_size']
+        sample_size = sample_plan_record['sample_size']
         sample_assay_plan.group_size = sample_size
         sample_assay_plan.add_sample_plan_record(sample_type, sample_size)
-        if 'assay_record_series' in sample_plan_params.keys():
-            for assay_plan_params in sample_plan_params['assay_record_series']:
-                tt = assay_plan_params['assay_type']['assay_type']
-                if tt == 'nmr spectroscopy':
-                    nmr_assay_type = AssayType(
-                        measurement_type='metabolite profiling',
-                        technology_type='nmr spectroscopy')
-                    from isatools.create.models import NMRTopologyModifiers
-                    nmr_top_mods = NMRTopologyModifiers()
-                    nmr_top_mods.technical_replicates = assay_plan_params[
-                        'assay_type']['acq_mod_cond']['technical_replicates']
-                    nmr_top_mods.acquisition_modes.add(
-                        assay_plan_params['assay_type']['acq_mod_cond'][
-                            'acq_mod'])
-                    nmr_top_mods.instruments.add('{} {}'.format(
-                        assay_plan_params['assay_type']['acq_mod_cond'][
-                            'nmr_instrument'],
-                        assay_plan_params['assay_type']['acq_mod_cond'][
-                            'magnet']))
-                    nmr_top_mods.pulse_sequences.add(
-                        assay_plan_params['assay_type']['acq_mod_cond'][
-                            'pulse_seq']
-                    )
-                    nmr_top_mods.magnet_power = \
-                    assay_plan_params['assay_type']['acq_mod_cond'][
-                        'magnet']
-                    nmr_assay_type.topology_modifiers = nmr_top_mods
-                    sample_assay_plan.add_assay_type(nmr_assay_type)
-                    sample_assay_plan.add_assay_plan_record(sample_type,
-                                                            nmr_assay_type)
-                elif tt == 'mass spectrometry':
-                    ms_assay_type = AssayType(
-                        measurement_type='metabolite profiling',
-                        technology_type='mass spectrometry')
-                    ms_assay_type.topology_modifiers = MSTopologyModifiers(
-                        sample_fractions=set(
-                            map(lambda x: x['fraction'], assay_plan_params[
-                                'assay_type']['samp_frac_series'])))
-                    injection_modes = ms_assay_type.topology_modifiers.injection_modes
-                    if len(assay_plan_params['assay_type'][
-                               'inj_mod_series']) > 0:
-                        for inj_mod in assay_plan_params['assay_type'][
-                                'inj_mod_series']:
-                            injection_mode = MSInjectionMode(
-                                injection_mode=inj_mod['inj_mod_cond'][
-                                    'inj_mod'],
-                                ms_instrument=inj_mod['inj_mod_cond'][
-                                    'instrument']
-                            )
-                            if inj_mod['inj_mod_cond']['inj_mod'] in (
-                                'LC', 'GC'):
-                                injection_mode.chromatography_instrument = \
-                                    inj_mod['inj_mod_cond']['chromato']
-                                if inj_mod['inj_mod_cond']['inj_mod'] == 'LC':
-                                    injection_mode.chromatography_column = \
-                                        inj_mod['inj_mod_cond']['chromato_col']
-                            injection_modes.add(injection_mode)
-                            for acq_mod in inj_mod['inj_mod_cond'][
-                                    'acq_mod_series']:
-                                injection_mode.acquisition_modes.add(
-                                    MSAcquisitionMode(
-                                        acquisition_method=acq_mod['acq_mod'],
-                                        technical_repeats=acq_mod[
-                                            'technical_replicates']
-                                    )
-                                )
-                                if inj_mod['inj_mod_cond']['inj_mod'] == 'GC':
-                                    for deriva in inj_mod['inj_mod_cond'][
-                                            'derivatization_series']:
-                                        injection_mode.derivatizations.add(
-                                            deriva['derivatization'])
-                    sample_assay_plan.add_assay_type(ms_assay_type)
-                    sample_assay_plan.add_assay_plan_record(sample_type,
-                                                            ms_assay_type)
-                else:
-                    raise NotImplementedError('Only MS assays supported')
-        for qc_plan_params in galaxy_parameters['qc_plan']['qc_record_series']:
-            if 'interval series' == qc_plan_params['qc_type_conditional'][
-                    'qc_type']:
-                sample_assay_plan.add_sample_qc_plan_record(
-                    material_type=qc_plan_params['qc_type_conditional'][
-                        'qc_material_type'],
-                    injection_interval=qc_plan_params['qc_type_conditional'][
-                        'injection_freq'])
-            elif 'dilution series' in qc_plan_params['qc_type_conditional'][
-                    'qc_type']:
-                raise NotImplementedError(
-                    'Dilution series QCs not yet supported!')
-        sample_assay_plan.group_size = \
-            galaxy_parameters['treatment_plan']['study_groups']['study_group_size']
+        for assay_plan_record in sample_plan_record['assay_record_series']:
+            tt = assay_plan_record['assay_type']['assay_type']
+            if tt == 'nmr spectroscopy':
+                assay_type = _create_nmr_assay_type(assay_plan_record)
+            elif tt == 'mass spectrometry':
+                assay_type = _create_ms_assay_type(assay_plan_record)
+            else:
+                raise NotImplementedError('Only MS and NMR assays supported')
+            sample_assay_plan.add_assay_type(assay_type)
+            sample_assay_plan.add_assay_plan_record(sample_type,assay_type)
+        return sample_assay_plan
+
+    def _inject_qcqa_plan(sample_assay_plan, qcqa_record):
+        if 'interval series' == qcqa_record['qc_type_conditional']['qc_type']:
+            sample_assay_plan.add_sample_qc_plan_record(
+                material_type=qcqa_record['qc_type_conditional']['qc_material_type'],
+                injection_interval=qcqa_record['qc_type_conditional']['injection_freq'])
+        elif 'dilution series' in qcqa_record['qc_type_conditional']['qc_type']:
+            raise NotImplementedError('Dilution series QCs not yet supported!')
+
+    # pre-generation checks
+    if galaxy_parameters_file:
+        galaxy_parameters = json.load(galaxy_parameters_file)
+        # print(json.dumps(galaxy_parameters, indent=4))  # fo debugging only
+    else:
+        raise IOError('Could not load Galaxy parameters file!')
+    if target_dir:
+        if not os.path.exists(target_dir):
+            raise IOError('Target path does not exist!')
+    if len(galaxy_parameters['sampling_and_assay_plans']) == 0:
+        raise IOError('No Sampling plan specified')
+
+    treatment_sequence = _create_treatment_sequence(galaxy_parameters)
+    sample_assay_plan = SampleAssayPlan()
+    for sample_plan_record in galaxy_parameters['sampling_and_assay_plans']['sample_record_series']:
+        _ = _create_sample_plan(sample_assay_plan, sample_plan_record)
+    for qcqa_record in galaxy_parameters['qc_plan']['qc_record_series']:
+        _ = _inject_qcqa_plan(sample_assay_plan, qcqa_record)
+    sample_assay_plan.group_size = galaxy_parameters['treatment_plan']['study_groups']['study_group_size']
 
     study_info = galaxy_parameters['study_overview']
 
@@ -234,6 +219,7 @@ def create_from_galaxy_parameters(galaxy_parameters_file, target_dir):
     i.identifier = s.identifier
     i.studies = [s]
 
+    # generate empty data file stubs
     for assay in s.assays:
         for data_file in assay.data_files:
             data_file_path = os.path.join(target_dir, data_file.filename)
