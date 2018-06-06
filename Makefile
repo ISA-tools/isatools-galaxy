ed8588bb6f12fba373be7f4c8f5261e7aa0658b1
Pierrick Roger <pk.roger@icloud.com>
2018-06-06 17:49:32 +0100

    Check identifiers in isacreator output.

--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ test:
 	bash-testthat/testthat.sh tests
 
 clean:
+	$(RM) -r tests/*.output
 
 .PHONY: all test clean
--- tests/test-isacreator.sh
+++ tests/test-isacreator.sh
@@ -14,7 +14,7 @@ RESDIR=$SCRIPT_PATH/res
 test_isacreator_fails_if_no_output_dir() {
 
 	local inputs_file="$RESDIR/isacreator_metabo_default_inputs.json"
-	local output_dir="$SCRIPT_PATH/test_isacreator_default_inputs.output"
+	local output_dir="$SCRIPT_PATH/test_isacreator_fails_if_no_output_dir.output"
 
 	[[ -d $output_dir ]]  && rm -r "$output_dir"
 
@@ -35,6 +35,14 @@ test_isacreator_default_inputs() {
 
 	mkdir "$output_dir"
 	expect_success "$ISACREATOR" "--galaxy_parameters_file=$inputs_file" "--target_dir=$output_dir" || return 1
+
+	# Check identifiers
+	investigation_file="$output_dir/i_investigation.txt"
+	expect_non_empty_file "$investigation_file"
+	investigation_id=$(grep '^Investigation Identifier' "$investigation_file" | sed 's/^.*\(ISA[^ ]*\)$/\1/')
+	study_id=$(grep '^Study Identifier' "$investigation_file" | sed 's/^.*\(ISA[^ ]*\)$/\1/')
+	expect_str_not_null "$investigation_id"
+	expect_str_eq "$study_id" "$investigation_id"
 }
 
 # Main {{{1
