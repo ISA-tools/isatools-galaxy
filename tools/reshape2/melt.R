2f81dd089917317b22da3453565d4e4b19aef30a
djcomlab <djcomlab@gmail.com>
2018-06-19 15:43:36 +0100

    Add reshape2 from https://github.com/galaxyproject/tools-iuc/blob/master/tools/reshape2/ and modify to use external R scripts

---
+++ tools/reshape2/melt.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+library(reshape2)
+library(optparse)
+
+# parse parameters
+option_list = list(
+  make_option(c("-i", "--input"), type="character", default=NULL, help="A path to a file to melt"),
+  make_option(c("-o", "--output"), type="character", default=file.path(getwd(), "output.tabular"), help="A path to an output")
+)
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+if (is.null(opt$input)) {
+    print_help(opt_parser)
+    stop("Please provide a path to an input file to melt (-i)", call.=FALSE)}
+
+
+options(show.error.messages=F, error=function(){cat(geterrmessage(),file=stderr());q("no",1,F)})
+loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+input <- read.csv(opt$input, sep='\t', header=TRUE)
+minput <- melt(input)
+write.table(minput, opt$output, sep="\t", quote=FALSE, row.names=FALSE)
