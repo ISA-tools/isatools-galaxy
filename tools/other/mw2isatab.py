9144b71e3d3371a15341045d6a3ce5f1870a1848
djcomlab <djcomlab@gmail.com>
2018-06-21 17:21:47 +0100

    Add mw2isatab tool

---
+++ tools/other/mw2isatab.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) < 3:
+   print('Usage: run_mwtab2isa.py mwtab_study_id target_dir')
+   sys.exit(0)
+
+mwtab_study_id = sys.argv[1]
+target_dir = sys.argv[2]
+try:
+    from isatools.net.mw2isa import mw2isa_convert
+    mw2isa_convert(studyid=mwtab_study_id, outputdir=target_dir)
+except ImportError as e:
+    raise RuntimeError('Could not import isatools package')
