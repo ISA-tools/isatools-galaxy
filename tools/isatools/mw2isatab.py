e5f6df24cafb6b36593a9e8f176c5e847bb65dc8
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2019-10-07 12:58:18 +0100

    Small simplifications

--- tools/isatools/mw2isatab.py
+++ tools/isatools/mw2isatab.py
@@ -2,15 +2,12 @@
 
 import sys
 
+from isatools.net.mw2isa import mw2isa_convert
+
 if len(sys.argv) < 3:
     print('Usage: run_mwtab2isa.py mwtab_study_id target_dir')
     sys.exit(0)
 
 mwtab_study_id = sys.argv[1]
 target_dir = sys.argv[2]
-print("TARGET:", target_dir)
-try:
-    from isatools.net.mw2isa import mw2isa_convert
-    mw2isa_convert(studyid=mwtab_study_id, outputdir=target_dir)
-except ImportError:
-    raise RuntimeError('Could not import isatools package')
+mw2isa_convert(studyid=mwtab_study_id, outputdir=target_dir)
